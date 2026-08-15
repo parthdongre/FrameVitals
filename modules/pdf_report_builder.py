b038ab497fc94bcc62389721c0ca97e3fde0346d
Parthozy <163404890+parthdongre@users.noreply.github.com>
2026-08-15 19:13:37 +0530

    Remove legacy modules compatibility namespace

--- modules/pdf_report_builder.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible PDF report builder imports.
-
-Deprecated: use framevitals.pdf_report_builder instead.
-"""
-
-from framevitals.pdf_report_builder import generate_pdf_report
-
-__all__ = ["generate_pdf_report"]
