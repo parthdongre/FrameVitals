990d49d2386c292d8231e70f423151690e7f7950
Parthozy <163404890+parthdongre@users.noreply.github.com>
2026-09-05 21:25:21 +0530

    release: expose FrameVitals 0.3.0 version

--- src/framevitals/__init__.py
+++ src/framevitals/__init__.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
     from framevitals.checks import DataCheck
     from framevitals.cleaning_plan import CleaningPlan
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def __getattr__(name: str):
